8a1b8f37cf4c66bf93547514da5e3139196ab678
venkatesh@office46 <venkatesh9036504739@gmail.com>
2023-08-24 12:57:08 +0530

    set

--- add num.py
+++ add num.py
@@ -22,3 +22,13 @@ s = (a + b + c) / 2
 
 
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+
+
+student_id = {112, 114, 116, 118, 115}
+print('Student ID:', student_id
+vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+print('Vowel Letters:', vowel_letters)
+
+
+mixed_set = {'Hello', 101, -2, 'Bye'}
+print('Set of mixed data types:', mixed_set)
