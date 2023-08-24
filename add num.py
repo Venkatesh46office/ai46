3c19e7881a75c11c8256c07e00bc1d2e71b921d3
venkatesh@office46 <venkatesh9036504739@gmail.com>
2023-08-24 12:47:49 +0530

    traingle

--- add num.py
+++ add num.py
@@ -6,3 +6,19 @@ sum = num1 + num2
 
 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+
+#triangle
+
+
+
+
+a = 5
+b = 6
+c = 7
+
+s = (a + b + c) / 2
+
+
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
