dbc37f6b12db9ac4b51838351f26566a74d7df12
venkatesh@office46 <venkatesh9036504739@gmail.com>
2023-09-07 10:57:28 +0530

    scatter

---
+++ example.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+a=200
+b=300
+if a>b:
+    print("a is great than b")
+else print("b is great than b")
---
+++ scling.py
@@ -0,0 +1,8 @@
+b="venkatesh"
+print(b[2:])
+
+
+
+age = 36
+txt = "My name is John, I am " + age
+print(txt) 
